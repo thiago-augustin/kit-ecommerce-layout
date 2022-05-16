53936d9136d3e2e374a1c27083f04cb103509640
thiago-augustin <75146536+thiago-augustin@users.noreply.github.com>
2022-05-16 14:50:51 -0300

    Adding completed order page and added redirect links

---
+++ js/redirect.js
@@ -0,0 +1,24 @@
+function complete() {
+    console.log("Redirecionado");
+    window.location.href = 'http://127.0.0.1:5500/complete.html?#';
+}
+
+function welcome() {
+    console.log("Redirecionado");
+    window.location.href = 'http://127.0.0.1:5500/welcome.html?#';
+}
+
+function authentication() {
+    console.log("Redirecionado");
+    window.location.href = 'http://127.0.0.1:5500/authentication.html?#';
+}
+
+function home() {
+    console.log("Redirecionado");
+    window.location.href = 'http://127.0.0.1:5500/home.html?#';
+}
+
+function basket() {
+    console.log("Redirecionado");
+    window.location.href = 'http://127.0.0.1:5500/order.html?#';
+}
